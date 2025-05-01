36bef57db9c16634101fc5c55bbb71e3668367b6
keeeeepin <ducknuttt@gmail.com>
2025-05-01 22:24:10 +0900

    fix : getWorkspaceList API

--- src/apis/api/workspace.js
+++ src/apis/api/workspace.js
@@ -1,10 +1,13 @@
-import { defaultApiInstance } from "@apis/utils"
+import { defaultApiInstance } from '@apis/utils';
 
 export const postCreateWorkspace = async (workspaceInfo) => {
-    return await defaultApiInstance.post("/workspace", workspaceInfo);
+  return await defaultApiInstance.post('/workspace', workspaceInfo);
 };
 
-export const getWorkspaceList = async () => {
-    return await defaultApiInstance.get("/workspace");
-}
+// export const postCreateWorkspace = async (workspaceId, workspaceInfo) => {
+//   return await defaultApiInstance.post(`/workspaces/${workspaceId}/member`, workspaceInfo);
+// };
 
+export const getWorkspaceList = async () => {
+  return await defaultApiInstance.get('/workspaces');
+};
