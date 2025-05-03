7d24a216db48ba80c06c4765e96624dfcb9cf692
keeeeepin <ducknuttt@gmail.com>
2025-05-03 17:27:15 +0900

    fix : 사용자 이메일 추가
    - Chat형식에 사용자 이메일 추가

--- src/components/Chat/ChatMessage.jsx
+++ src/components/Chat/ChatMessage.jsx
@@ -1,17 +1,15 @@
-import { HStack, Box, Text } from "@chakra-ui/react";
+import { HStack, Box, Text } from '@chakra-ui/react';
 
-
-const ChatMessage = ({msg}) => {
-
-
+const ChatMessage = ({ msg }) => {
   return (
     <>
       <HStack alignItems="start" p={2} _hover={{ bg: 'gray.700' }}>
         {/* <Avatar size="sm" name={`User ${msg}`} src={`/api/placeholder/32/32`} /> */}
-        <Box pl={2} color="#F2F3F5" >
+        <Box pl={2} color="#F2F3F5">
           <HStack spacing={2}>
-            <Text fontWeight="bold" fontSize="15px">{msg.name}</Text>
-
+            <Text fontWeight="bold" fontSize="15px">
+              {msg.memberName}
+            </Text>
 
             <Text fontSize="12px" color="gray.500">
               {msg.createdAt}
