57b0d20981df581fb13c56a801581ebc9fd1961c
keeeeepin <ducknuttt@gmail.com>
2025-05-06 14:47:12 +0900

    feat : websocket url 포트 변경

--- src/hooks/useSocketConnection.jsx
+++ src/hooks/useSocketConnection.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 import { useEffect, useState } from 'react';
 import { Client } from '@stomp/stompjs';
@@ -7,47 +7,43 @@ import useStompClientHooks from './useStompClientHooks';
 import useSocketClientStore from '../store/useSocketClientStore';
 
 const useSocketConnection = () => {
+  const { setClient, setIsConnected } = useSocketClientStore();
+  const { deactivateSocket } = useStompClientHooks();
 
-    const { setClient, setIsConnected } = useSocketClientStore();
-    const { deactivateSocket } = useStompClientHooks();
+  useEffect(() => {
+    const client = new Client({
+      brokerURL: 'ws://localhost:8090/ws',
+      connectHeaders: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
 
+      reconnectDelay: 5000,
+      heartbeatIncoming: 4000,
+      heartbeatOutgoing: 4000,
 
-    useEffect(() => {
-            const client = new Client({
-                brokerURL: 'ws://localhost:8080/ws',
-                connectHeaders: {
-                  Authorization: `Bearer ${localStorage.getItem('token')}`
-                },
-          
-                reconnectDelay: 5000,
-                heartbeatIncoming: 4000,
-                heartbeatOutgoing: 4000,
-          
-                onConnect: function () {
-                  setIsConnected(true); // FIX 
-                  console.log('Connected!');
-                },
-                onDisconnect: function () {
-                  setIsConnected(false);
-                  console.log('Disconnected!');
-                },
-                debug: (str) => {
-                  console.log(`STOMP Debug: ${str}`);
-                },
-                onStompError: function (frame) {
-                  console.log('Error: ' + frame.headers['message']);
-                  console.log('Details: ' + frame.body);
-                },
-              });
-              client.activate();
-              setClient(client); 
+      onConnect: function () {
+        setIsConnected(true); // FIX
+        console.log('Connected!');
+      },
+      onDisconnect: function () {
+        setIsConnected(false);
+        console.log('Disconnected!');
+      },
+      debug: (str) => {
+        console.log(`STOMP Debug: ${str}`);
+      },
+      onStompError: function (frame) {
+        console.log('Error: ' + frame.headers['message']);
+        console.log('Details: ' + frame.body);
+      },
+    });
+    client.activate();
+    setClient(client);
 
-        return () => {
-            deactivateSocket(); 
-        }
-    
-      }, [setClient, setIsConnected]);
+    return () => {
+      deactivateSocket();
+    };
+  }, [setClient, setIsConnected]);
+};
 
-}
-
-export default useSocketConnection
+export default useSocketConnection;
