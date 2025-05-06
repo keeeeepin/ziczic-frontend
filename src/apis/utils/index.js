28ccb030c57db3ef3331101335e06f75351fbad7
keeeeepin <ducknuttt@gmail.com>
2025-05-06 14:30:10 +0900

    feat : BASE_URL 포트 번호 변경

--- src/apis/utils/index.js
+++ src/apis/utils/index.js
@@ -1,38 +1,38 @@
-import  axios from 'axios';
+import axios from 'axios';
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = 'http://localhost:8090';
 
-const axiosApi =  (url, options) => {
-    const instance = axios.create({baseURL: url, ...options});
+const axiosApi = (url, options) => {
+  const instance = axios.create({ baseURL: url, ...options });
 
-    instance.interceptors.request.use(
-        (config) => {
-            const accessToken = localStorage.getItem('token')
-            config.headers['Content-Type'] = 'application/json';
-            if (accessToken) {
-                config.headers['Authorization'] = `Bearer ${accessToken}`;
-            }
-            return config; 
-        },
-        (error) => {
-            return Promise.reject(error); 
-        }
-    )
+  instance.interceptors.request.use(
+    (config) => {
+      const accessToken = localStorage.getItem('token');
+      config.headers['Content-Type'] = 'application/json';
+      if (accessToken) {
+        config.headers['Authorization'] = `Bearer ${accessToken}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
 
-    // Response
-    instance.interceptors.response.use(
-        (resp) => {
-            return resp;
-        },
-        (error) => {
-            if (error.response?.status == 404 ) {
-                console.log(error.response.data);
-            }
-            return Promise.reject(error); 
-        }
-    )
+  // Response
+  instance.interceptors.response.use(
+    (resp) => {
+      return resp;
+    },
+    (error) => {
+      if (error.response?.status == 404) {
+        console.log(error.response.data);
+      }
+      return Promise.reject(error);
+    }
+  );
 
-    return instance;
-}
+  return instance;
+};
 
 export const defaultApiInstance = axiosApi(BASE_URL);
